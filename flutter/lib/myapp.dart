e94f180553f1d4ba61bc11b4656c9acf06259cb4
Yuji Yamamoto <rr.yamamoto@gmail.com>
2022-02-12 22:25:19 +0900

    Firebase Analytics設定を追加

--- flutter/lib/myapp.dart
+++ flutter/lib/myapp.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
 import 'package:money_hacker/app_router.dart';
 import 'package:money_hacker/app_theme.dart';
@@ -5,14 +6,14 @@ import 'package:money_hacker/app_theme.dart';
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return DefaultTextStyle(
-      style: TextStyle(fontFamily: 'Noto Sans JP'),
-      child: MaterialApp(
-        title: 'MONEY HACKER',
-        theme: AppTheme.theme,
-        initialRoute: '/',
-        onGenerateRoute: AppRouter.onGenerateRoute,
-      ),
+    return MaterialApp(
+      title: 'MONEY HACKER',
+      theme: AppTheme.theme,
+      initialRoute: '/',
+      onGenerateRoute: AppRouter.onGenerateRoute,
+      navigatorObservers: [
+        FirebaseAnalyticsObserver(analytics: FirebaseAnalytics.instance),
+      ],
     );
   }
 }
